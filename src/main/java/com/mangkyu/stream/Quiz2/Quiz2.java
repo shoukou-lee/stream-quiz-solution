7a81980d7d8099828f7d8deb085c63d62b3cc65b
shoukou-lee <shoukou.lee@gmail.com>
2022-11-10 17:42:31 +0900

    add: quiz 1

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -5,10 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 아래와 같은 데이터가 저장된 리스트가 있다고 하자.
+ * ```java
+ * private final static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
+ * ```
+ */
 public class Quiz2 {
 
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
+    /**
+     * List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
+     * ex) ("T", 1), ("a", 2) ...
+     */
     public Map<String, Integer> quiz1() {
         return new HashMap<>();
     }
