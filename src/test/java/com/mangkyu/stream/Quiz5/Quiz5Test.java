ee69cb772b4c49cddb9e977a6606de7df4f5db64
shoukou-lee <shoukou.lee@gmail.com>
2022-11-11 11:19:59 +0900

    add: quiz 5

--- src/test/java/com/mangkyu/stream/Quiz5/Quiz5Test.java
+++ src/test/java/com/mangkyu/stream/Quiz5/Quiz5Test.java
@@ -32,6 +32,7 @@ class Quiz5Test {
     private boolean isCorrect(List<Integer> quiz) {
         List<Integer> answer = new ArrayList<>(quiz);
         Collections.sort(answer);
+        Collections.sort(quiz);
         return quiz.stream().distinct().count() == 6 && answer.equals(quiz);
     }
 
